f0ac368e3ad8d7867e626d76bcb7197c350924c2
azra-dev <azraelanonas@gmail.com>
2024-06-23 19:03:30 +0800

    Implemented sr-prototype.py

--- SETUP.md
+++ SETUP.md
@@ -1,5 +1,5 @@
 # Operating System ⚙️
-The Rock Pi 5 uses any Linux-based OS with any Desktop Environment. Recommended to use __Debian 12__ as distro and __KDE Plasma__ as DE.
+The Raspberry 4 uses any Linux-based OS with any Desktop Environment. Recommended to use __Debian 12__ as distro and __KDE Plasma__ as DE.
 
 # Setting Up System 🔧
 **Make yourself as a root**
@@ -18,36 +18,46 @@ The Rock Pi 5 uses any Linux-based OS with any Desktop Environment. Recommended 
 > Discord (optional)
 
 # Dependencies 💿
-## **In-Terminal**
+### **In-Terminal**
+- sudo apt-get update
+- sudo apt-get install build-essential
+- sudo apt-get install libgtk-3-dev
+- sudo apt-get install libboost-all-dev
+- sudo apt install libmpv-dev mpv
 
-sudo apt-get update
+### **Virtual  Environment**
 
-sudo apt-get install build-essential
+#### For Super-Resolution
+- pip install -r _gfppgan-package/requirements.txt
+- pip install basicsr>=1.4.2 facexlib>=0.2.5
+- python _gfpgan-package/setup.py develop
+- pip install realesrgan
+- wget https://github.com/TencentARC/GFPGAN/releases/download/v1.3.0/GFPGANv1.3.pth -P experiments/pretrained_models
 
-sudo apt-get install libgtk-3-dev
+#### For Face Detection
+- pip install pybind11 cmake==3.25.2
+- pip install dlib==19.24.2 face-recognition 
 
-sudo apt-get install libboost-all-dev
+#### For GUI
+- pip install flet==0.19.0
 
-sudo apt install libmpv-dev mpv
+# Error Handling 🔧
 
-## **Virtual  Environment**
+### Git Error
+During commit, sometimes an error of `RPC failed; HTTP 400 curl 22 The requested URL returned error: 400 Bad Request` will be shown likely due to committing something with a file size beyond 100MB. Run the command in the terminal to produce a .gitignore file to ignore large file size:
+```bash
+find . -size +100M | cat >> .gitignore
+```
+However, do remove the `./` prefix of each generated output to properly address the files to be ignored
 
-# For Super-Resolution
+### basicsr error
+Upon running the sr-prototype.py program, an error will be recieved which comes from basicsr module due to deprication. The module is modifiable thankfully with the following step:
 
-_Note: Make sure to clone https://github.com/TencentARC/GFPGAN first in a separate repository. Paste **requirements.txt**, **VERSION**, **setup.py**, **experiments** folder, and **gfpgan** folder into this repository afterwards. After installation, it can be deleted._
-
-pip install -r _gfppgan-package/requirements.txt
-pip install basicsr>=1.4.2 facexlib>=0.2.5
-python _gfpgan-package/setup.py develop
-pip install realesrgan
-wget https://github.com/TencentARC/GFPGAN/releases/download/v1.3.0/GFPGANv1.3.pth -P experiments/pretrained_models
-
-# For Face Detection
-
-pip install pybind11 cmake==3.25.2
-
-pip install dlib==19.24.2 face-recognition 
-
-# For GUI
-
-pip install flet==0.19.0
+Open `your_venv/lib/python3.11/site-packages/basicsr/data/degradations.py` and on line 8, simply change:
+```py
+from torchvision.transforms.functional_tensor import rgb_to_grayscale
+```
+to
+```py
+from torchvision.transforms.functional import rgb_to_grayscale
+```
--- sr-prototype.py
+++ sr-prototype.py
@@ -2,28 +2,27 @@ import cv2
 import numpy as np
 import os
 import torch
+from basicsr.utils import imwrite
 from gfpgan import GFPGANer
 
 class SR():
     # ATTRIBUTES
-    arch = 'original'
-    channel_multiplier = 1
-    model_name = 'GFPGANv1'
-    url = 'https://github.com/TencentARC/GFPGAN/releases/download/v0.1.0/GFPGANv1.pth'
     restorer = None
+    mode = 'single'
 
     def __init__(self,
                 input = 'tpdne_dataset/LR128',
-                output = 'results',
-                version = 1.3,
+                output = '_results',
+                version = '1.3',
                 upscale = 2,
                 bg_upsampler = 'realesrgan',
                 bg_tile = 400,
-                prefix = 'HR_',
+                prefix = 'HR',
                 suffix = None,
                 only_center_face = None,
                 aligned = None,
-                ext = 'auto'
+                ext = 'auto',
+                weight = 0.5
                 ):
         self.input = input
         self.output = output
@@ -36,6 +35,7 @@ class SR():
         self.only_center_face = only_center_face
         self.aligned = aligned
         self.ext = ext
+        self.weight = weight
     
     # BACKGROUND SAMPLER
     def run_background_sampler(self):
@@ -64,59 +64,122 @@ class SR():
     # CONFIGURE RESTORER
     def configure_restorer(self):
         # SET VERSION
+        arch = 'original'
+        channel_multiplier = 1
+        model_name = 'GFPGANv1'
+        url = 'https://github.com/TencentARC/GFPGAN/releases/download/v0.1.0/GFPGANv1.pth'
+
         if self.version == '1':
-            self.arch = 'original'
-            self.channel_multiplier = 1
-            self.model_name = 'GFPGANv1'
-            self.url = 'https://github.com/TencentARC/GFPGAN/releases/download/v0.1.0/GFPGANv1.pth'
+            arch = 'original'
+            channel_multiplier = 1
+            model_name = 'GFPGANv1'
+            url = 'https://github.com/TencentARC/GFPGAN/releases/download/v0.1.0/GFPGANv1.pth'
         elif self.version == '1.2':
-            self.arch = 'clean'
-            self.channel_multiplier = 2
-            self.model_name = 'GFPGANCleanv1-NoCE-C2'
-            self.url = 'https://github.com/TencentARC/GFPGAN/releases/download/v0.2.0/GFPGANCleanv1-NoCE-C2.pth'
+            arch = 'clean'
+            channel_multiplier = 2
+            model_name = 'GFPGANCleanv1-NoCE-C2'
+            url = 'https://github.com/TencentARC/GFPGAN/releases/download/v0.2.0/GFPGANCleanv1-NoCE-C2.pth'
         elif self.version == '1.3':
-            self.arch = 'clean'
-            self.channel_multiplier = 2
-            self.model_name = 'GFPGANv1.3'
-            self.url = 'https://github.com/TencentARC/GFPGAN/releases/download/v1.3.0/GFPGANv1.3.pth'
+            arch = 'clean'
+            channel_multiplier = 2
+            model_name = 'GFPGANv1.3'
+            url = 'https://github.com/TencentARC/GFPGAN/releases/download/v1.3.0/GFPGANv1.3.pth'
         elif self.version == '1.4':
-            self.arch = 'clean'
-            self.channel_multiplier = 2
-            self.model_name = 'GFPGANv1.4'
-            self.url = 'https://github.com/TencentARC/GFPGAN/releases/download/v1.3.0/GFPGANv1.4.pth'
+            arch = 'clean'
+            channel_multiplier = 2
+            model_name = 'GFPGANv1.4'
+            url = 'https://github.com/TencentARC/GFPGAN/releases/download/v1.3.0/GFPGANv1.4.pth'
         elif self.version == 'RestoreFormer':
-            self.arch = 'RestoreFormer'
-            self.channel_multiplier = 2
-            self.model_name = 'RestoreFormer'
-            self.url = 'https://github.com/TencentARC/GFPGAN/releases/download/v1.3.4/RestoreFormer.pth'
+            arch = 'RestoreFormer'
+            channel_multiplier = 2
+            model_name = 'RestoreFormer'
+            url = 'https://github.com/TencentARC/GFPGAN/releases/download/v1.3.4/RestoreFormer.pth'
         else:
             raise ValueError(f'Wrong model version {self.version}.')
         
-        model_path = os.path.join('experiments/pretrained_models', self.model_name + '.pth')
+        model_path = os.path.join('experiments/pretrained_models', model_name + '.pth')
         if not os.path.isfile(model_path):
-            model_path = os.path.join('gfpgan/weights', self.model_name + '.pth')
+            model_path = os.path.join('gfpgan/weights', model_name + '.pth')
         if not os.path.isfile(model_path):
             # download pre-trained models from url
-            model_path = self.url
+            model_path = url
 
         # SET RESTORER
         restorer = GFPGANer(
             model_path=model_path,
             upscale=self.upscale,
-            arch=self.arch,
-            channel_multiplier=self.channel_multiplier,
+            arch=arch,
+            channel_multiplier=channel_multiplier,
             bg_upsampler=self.bg_upsampler)
         
         self.restorer = restorer
     
     # UPSCALE / RESTORE
-    def upscale(self):
-        print('lorem ipsum dolor sit amet muna pre')
-        
+    def Upscale(self):
+        # read image
+        img_name = os.path.basename(self.input)
+        basename, ext = os.path.splitext(img_name)
+        input_img = cv2.imread(self.input, cv2.IMREAD_COLOR)
+
+        # restore faces and background if necessary
+        cropped_faces, restored_faces, restored_img = self.restorer.enhance(
+            input_img,
+            has_aligned=self.aligned,
+            only_center_face=self.only_center_face,
+            paste_back=True,
+            weight=self.weight)
+
+        # save faces
+        for idx, (cropped_face, restored_face) in enumerate(zip(cropped_faces, restored_faces)):
+            # save cropped face
+            save_crop_path = os.path.join(self.output, 'cropped_faces', f'{basename}_{idx:02d}.png')
+            imwrite(cropped_face, save_crop_path)
+            # save restored face
+            if (self.suffix is None and self.prefix is None):
+                save_face_name = f'{basename}_{idx:02d}.png'
+            else:
+                save_face_name = f'{basename}_{idx:02d}'
+                if self.suffix is not None:
+                    save_face_name = f'{save_face_name}_{self.suffix}'
+                if self.prefix is not None:
+                    save_face_name = f'{self.prefix}_{save_face_name}'
+                save_face_name = f'{save_face_name}.png'
+            
+            save_restore_path = os.path.join(self.output, 'restored_faces', save_face_name)
+            imwrite(restored_face, save_restore_path)
+
+            # save comparison image
+            cmp_img = np.concatenate((cropped_face, restored_face), axis=1)
+            imwrite(cmp_img, os.path.join(self.output, 'cmp', f'{basename}_{idx:02d}.png'))
+
+        # save restored img
+        if restored_img is not None:
+            if self.ext == 'auto':
+                extension = ext[1:]
+            else:
+                extension = self.ext
+
+            if (self.suffix is None and self.prefix is None):
+                save_restore_path = os.path.join(self.output, 'restored_imgs', f'{basename}.{extension}')
+            else:
+                restore_name = f'{basename}'
+                if self.suffix is not None:
+                    restore_name = os.path.join(self.output, 'restored_imgs', f'{restore_name}_{self.suffix}') 
+                if self.prefix is not None:
+                    restore_name = os.path.join(self.output, 'restored_imgs', f'{self.prefix}_{restore_name}') 
+                save_restore_path = os.path.join(self.output, 'restored_imgs', f'{restore_name}.{extension}') 
+            imwrite(restored_img, save_restore_path)
+    
+    def Run(self):
+        self.run_background_sampler()
+        self.configure_restorer()
+        self.Upscale()
+            
 
 
 def main():
-    sr_prototype = SR()
+    sr_prototype = SR(input = 'tpdne_dataset/LR128/3.png', upscale=8)
+    sr_prototype.Run()
 
 if __name__ == '__main__':
     main()
